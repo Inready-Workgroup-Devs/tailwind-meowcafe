94d570ea7c62457e948e6369f52f10218745afc1
abdurrahmanr <imanabdurrahmanr@gmail.com>
2024-05-24 15:48:21 +0800

    feat: dashboard, products, orders page

---
+++ src/js/imageUploader.js
@@ -0,0 +1,18 @@
+const img = document.getElementById("image-upload");
+const imgViewer = document.getElementById("image-viewer");
+const dragArea = document.getElementById("drag-area");
+img.addEventListener("change", uploadImage);
+
+function uploadImage() {
+	let link = URL.createObjectURL(img.files[0]);
+	imgViewer.style.backgroundImage = `url(${link})`;
+	imgViewer.textContent = "";
+	return;
+}
+
+dragArea.addEventListener("dragover", (e) => e.preventDefault());
+dragArea.addEventListener("drop", (e) => {
+	e.preventDefault();
+	img.files = e.dataTransfer.files;
+	uploadImage();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,29 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+		data: {
+			active: 'page~="active"',
+			complete: 'status~="complete"',
+			processing: 'status~="processing"',
+		},
+		fontSize: {
+			xs: "0.625rem",
+			sm: "0.75rem",
+			base: "0.875rem",
+			md: "1rem",
+			lg: "1.25rem",
+			xl: "1.5rem",
+			"2xl": "1.75rem",
+			"3xl": "2rem",
+		},
 		extend: {
+			height: {
+				content: "calc(100vh - 70px)",
+			},
 			colors: {
 				primary: "#4880FF",
+				success: "#00B69B",
+				backgroundPrimary: "#F5F6FA",
 			},
 			fontFamily: {
 				sans: ["Nunito Sans", "sans-serif"],
